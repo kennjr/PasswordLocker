bba8bfee3dd9d7d5fcd3fdda8db76203d2fbb11a
kennjr <ken.juniour@student.moringaschool.com>
2022-02-20 09:49:18 +0300

    Added the fun.s needed for the user class.

---
+++ models/User.py
@@ -0,0 +1,58 @@
+
+
+class User:
+    users_list = []
+
+    def __init__(self, name, password, datetime):
+        self.name = name
+        self.password = password
+        self.datetime = datetime
+
+    def new_user(self):
+        """
+        Add a new user to the users_list
+        :return:
+        """
+        User.users_list.append(self)
+
+    # The del fun. will delete the contact from all the instances of the class that's why it's a class method
+    @classmethod
+    def delete_user(cls):
+        """
+        Delete a user (that was passed as a param) from the users list
+        :return:
+        """
+        User.users_list.remove(cls)
+
+    # CLs indicates that the method is part of the class i.e Contact
+    @classmethod
+    def does_user_exist(cls, name):
+        """
+        Check whether a user exists based on the name param passed
+        :param name:
+        :return: boolean
+        """
+        for user in cls.users_list:
+            if user.name == name:
+                return True
+
+        return False
+
+    @classmethod
+    def display_users(cls):
+        """
+        method that returns the users list
+        """
+        return cls.users_list
+
+    # The fun below will
+    @classmethod
+    def find_user_by_name(cls, name):
+        """
+        This fun. will return a user obj if one is found that matches the name arg
+        """
+        for user in cls.users_list:
+            if user.name == name:
+                return user
+
+
