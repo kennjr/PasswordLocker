1e914b7463ea2574cc164eb984d6625a4d78e7d7
kennjr <ken.juniour@student.moringaschool.com>
2022-02-20 17:42:04 +0300

    Added the if statement for displaying the user's credentials

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
 import login2
+import locker
 
 
 def login_interaction():
@@ -25,8 +26,9 @@ def login_attempt(name, password):
         else:
             print("The name and password don't match. Please try again")
     else:
-        login2.save_user(login2.create_user(name, password))
-        login2.update_current_user_val(user2)
+        new_user = login2.create_user(name, password)
+        login2.save_user(new_user)
+        login2.update_current_user_val(new_user)
         locker_interaction()
 
 
@@ -56,12 +58,21 @@ def locker_interaction():
                 password = str(input())
 
             owner = login2.get_current_user_val()
-
-            # save_contact(create_contact(f_name, l_name, p_number, e_address))  # create and save new contact.
+            locker.add_credentials(locker.create_credential(application, username, password, owner.name))  # create and save new contact.
             print(f"The new credentials have been created: {application}, {username}, {password} for {owner.name}")
             print('\n')
             print(f"New Credentials for {application} created")
             print('\n')
+        elif action_str == "dis-cred":
+            owner = login2.get_current_user_val()
+            cred_list = locker.get_all_credentials(owner.name)
+            print(f"The owner is {owner.name}")
+            if cred_list != []:
+                print(f"""|Application || Username | Password|""")
+                for credential in cred_list:
+                    print(f"""|{credential.application} || {credential.username} | {credential.password}|""")
+            else:
+                print("You've not yet saved any credentials in locker")
 
 
 def do_passwords_match(code_1, code_2):
