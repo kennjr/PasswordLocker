43e077906a577958e91748e60d18753c2b0aa8ee
kennjr <ken.juniour@student.moringaschool.com>
2022-02-20 11:19:40 +0300

    Created a replica of the login file outside the models scope for ease of working and added the find_user fun.

---
+++ login2.py
@@ -0,0 +1,42 @@
+from user2 import User2
+import datetime
+
+
+def create_user(name, password):
+    """
+    Create a user obj for the user who's trynna create an acc.
+    :param name:
+    :param password:
+    :return:
+    """
+    current_time = datetime.datetime.now()
+    time_str = str(current_time)
+    new_user = User2(name, password, time_str)
+    return new_user
+
+
+def save_user(new_user):
+    """
+    Save the user that's passed as a param
+    :param new_user:
+    :return:
+    """
+    new_user.save_user()
+
+
+def del_user(user):
+    """
+    Delete the user that's been passed as a param
+    :param user:
+    :return:
+    """
+    user.delete_user()
+
+
+def find_user(name):
+    """
+    Search for user using the search str passed
+    :param name:
+    :return:
+    """
+    return User2.does_user_exist(name)
