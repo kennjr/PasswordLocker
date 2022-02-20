3b0684f1a1cc0d55dbbcf4ae4b42dcdf29fbb10f
kennjr <ken.juniour@student.moringaschool.com>
2022-02-20 10:22:09 +0300

    Added the first 2 fun.s that we'll use for logging a user into the sys.

--- models/Login.py
+++ models/Login.py
@@ -1,9 +1,25 @@
+from user import User
+import datetime
 
 
-class Login:
+def create_user(name, password):
+    """
+    Create a user obj for the user who's trynna create an acc.
+    :param name:
+    :param password:
+    :return:
+    """
+    current_time = datetime.datetime.now()
+    time_str = str(current_time)
+    new_user = User(name, password, time_str)
+    return new_user
 
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
 
-    # def login_attempt(self):
+def save_user(new_user):
+    """
+    Save the user that's passed as a param
+    :param new_user:
+    :return:
+    """
+    new_user.save_user()
+
