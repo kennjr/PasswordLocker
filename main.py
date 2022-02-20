b4b7ce453cfe7f9a5e30fdda51ffecd564432918
kennjr <ken.juniour@student.moringaschool.com>
2022-02-20 15:04:28 +0300

    Added the password check for when a user is trynna login

--- main.py
+++ main.py
@@ -9,23 +9,33 @@ def login_interaction():
             name = str(input("Name: "))
             password = str(input("Password: "))
 
-            login2.save_user(login2.create_user(name, password))
-            user2 = login2.find_user(name)
-            login2.update_current_user_val(user2)
-            print(login2.get_current_user_val().name)
-
-            print(login2.check_for_existing_user(name))
-            locker_interaction()
+            login_attempt(name, password)
         elif login_str.lower() == "n":
             break
+
+
+def login_attempt(name, password):
+    user2 = login2.find_user(name)
+    if user2 is not None:
+        password_check = do_passwords_match(user2.password, password)
+        if password_check:
+            login2.update_current_user_val(user2)
+            print(f"Welcome back {name}")
+            locker_interaction()
+        else:
+            print("The name and password don't match. Please try again")
+    else:
+        login2.save_user(login2.create_user(name, password))
+        login2.update_current_user_val(user2)
+        locker_interaction()
 
 
 def locker_interaction():
     while True:
         print("""Use these codes to interact with passwordlocker - new-cred :to add new credentials
-                                                                   del-cred :to delete credentials
-                                                                   dis-cred :to display credentials
-                                                                   login :to login as diff. user""")
+                                                               del-cred :to delete credentials
+                                                               dis-cred :to display credentials
+                                                               login :to login as diff. user""")
         action_str = str(input("")).lower()
         if action_str == 'new-cred':
             print("New Credentials")
@@ -52,6 +62,13 @@ def locker_interaction():
             print('\n')
             print(f"New Credentials for {application} created")
             print('\n')
+
+
+def do_passwords_match(code_1, code_2):
+    if code_1 == code_2:
+        return True
+    else:
+        return False
 
 
 def generate_password():
