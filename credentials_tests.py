85531da293344bab013d7355aec775d371ef86c4
kennjr <ken.juniour@student.moringaschool.com>
2022-02-21 10:26:51 +0300

    Created fun.s for all the other cred methods (that are in the class) and tested them

--- credentials_tests.py
+++ credentials_tests.py
@@ -22,6 +22,47 @@ class MyTestCase(unittest.TestCase):
         # Compare the old length and new length of the list using the assertEqual
         self.assertEqual(len(Credentialz.credentials_list), length + 1)
 
+    def test_delete_a_user(self):
+        """
+        Delete a credentials obj form the list and check whether it returns the array-1
+        :return:
+        """
+        self.new_credentials.new_credentials()
+        new_user_2 = Credentialz("twitter", "userone", "1234", "user1")
+        new_user_2.new_credentials()
+        initial_length = len(Credentialz.credentials_list)
+        new_user_2.delete_credential_from_instance()
+        self.assertEqual(len(Credentialz.credentials_list), initial_length - 1)
+
+    def test_display_users(self):
+        """
+        Get all the credentials in the list and return them
+        :return:
+        """
+        self.new_credentials.new_credentials()
+        self.assertEqual(Credentialz.owners_credentials("user1"), Credentialz.credentials_list)
+
+    def test_find_cred_by_app_name(self):
+        """
+        Get credentials based on app_name that's been passed
+        :return:
+        """
+        self.new_credentials.new_credentials()
+        new_user_2 = Credentialz("twitter", "userone", "1234", "user1")
+        new_user_2.new_credentials()
+        self.assertEqual(new_user_2, Credentialz.find_credentials_by_app_name("twitter", "user1"))
+
+    def test_do_cred_exist(self):
+        """
+        Check whether a cred that was added exists
+        :return:
+        """
+        self.new_credentials.new_credentials()
+        new_user_2 = Credentialz("twitter", "userone", "1234", "user1")
+        new_user_2.new_credentials()
+        self.assertTrue(Credentialz.do_credentials_exist("twitter", "user1"))
+
+
 
 
 if __name__ == '__main__':
